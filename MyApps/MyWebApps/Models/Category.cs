208a8e19161ef091c4cb7ac9dca89d4326c5f480
Arafat <arafat97alim@gmail.com>
2022-05-10 01:12:56 +0530

    Added Category Model and required setting.

---
+++ MyApps/MyWebApps/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebApps.Models
+{
+    public class Category
+    {
+        //properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now();
+    }
+}
