177d68e7d7e02d1ba1a6e7f5670a562a97a0f3ab
Arafat <arafat97alim@gmail.com>
2022-05-10 02:11:26 +0530

    register data context in program file and sqlserver package added.

---
+++ MyApps/MyWebApps/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApps.Models;
+
+namespace MyWebApps.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //creating a constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
--- MyApps/MyWebApps/Program.cs
+++ MyApps/MyWebApps/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyWebApps.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//registering our application context
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
