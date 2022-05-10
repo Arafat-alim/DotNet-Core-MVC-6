554f93956e8b5b2d06b590b4b63baf3e454d56fb
Arafat <arafat97alim@gmail.com>
2022-05-11 00:08:42 +0530

    data retrive from the database.

--- MyApps/MyWebApps/Controllers/CategoryController.cs
+++ MyApps/MyWebApps/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApps.Data;
+using MyWebApps.Models;
 
 namespace MyWebApps.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //list of data coming from the database.
+            IEnumerable<Category> categories = _context.categories;
+            return View(categories); // data sending to view Category
         }
     }
 }
