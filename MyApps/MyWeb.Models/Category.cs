094579457b952f06c2cb3099f2ebdc5bd4b8c7c3
Arafat <arafat97alim@gmail.com>
2022-05-12 01:30:49 +0530

    Added Model (Bussiness Logic layer) for seperation.

---
+++ MyApps/MyWeb.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWeb.Models
+{
+    public class Category
+    {
+        //properties
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+        //jab bhi apna order add hoga toh uska datetime add hojyga 
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
