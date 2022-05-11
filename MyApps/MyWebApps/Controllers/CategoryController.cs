aca9b3edbfe54e31d0327a6479617c4d58ba59fb
Arafat <arafat97alim@gmail.com>
2022-05-12 01:14:28 +0530

    Added notification, CRUD operatioin.

--- MyApps/MyWebApps/Controllers/CategoryController.cs
+++ MyApps/MyWebApps/Controllers/CategoryController.cs
@@ -19,11 +19,100 @@ namespace MyWebApps.Controllers
             IEnumerable<Category> categories = _context.categories;
             return View(categories); // data sending to view Category
         }
-        //creating a Post Request
+        //creating a Get Request
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        //creating a Post Request adding
+        public IActionResult Create(Category category) //yeh category form se ayega
+        {
+            if(ModelState.IsValid)
+            {
+                _context.categories.Add(category); //Form se aya data add kar diya database
+                _context.SaveChanges(); //database ko save kardiya
+                TempData["success"] = "Created Data Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+            
+        }
+        //creating a request for updating
+        //creating a Get Request
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id ==  null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                //id nikaalenge
+                var category = _context.categories.Find(id);
+                if(category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(category);
+                }                
+            }
+        }
+        //creating a Post Request adding
+        [HttpPost]
+        public IActionResult Edit(Category category) //yeh category form se ayega
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(category); //Form se aya data add kar diya database
+                _context.SaveChanges(); //database ko save kardiya
+                TempData["success"] = "Edited Data Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+
+        //Delete Request
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                //id nikaalenge
+                var category = _context.categories.Find(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(category);
+                }
+            }
+        }
+        //creating a Post Request adding
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteData(int? id) //yeh category form se ayega
+        {
+            //category nikaalenge
+            var category = _context.categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            _context.categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Delete Data Successfully!";
+            return RedirectToAction("Index");
+
         }
     }
 }
--- MyApps/MyWebApps/Models/Category.cs
+++ MyApps/MyWebApps/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyWebApps.Models
 {
@@ -9,6 +10,7 @@ namespace MyWebApps.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
         //jab bhi apna order add hoga toh uska datetime add hojyga 
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
