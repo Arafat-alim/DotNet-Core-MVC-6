4fc74ec80bd8cd1b34bb991bfa11f7c9b3f2eb1f
Arafat <arafat97alim@gmail.com>
2022-05-14 22:49:10 +0530

    added repository pattern Beginner part

---
+++ MyApps/MyWeb.DataAccessLayer/Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeb.DataAccessLayer.Infrastructure.IRepository
+{
+    public interface IRepository<T> where T : class //T jo hai wo koi class aa skti hai
+    {
+        IEnumerable<T> GetAll();
+
+        T GetT(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+
+        void DeleteRange(IEnumerable<T> entity); //multiple data delete kr skte hai
+        // ek class me multiple list aa sakte h
+    }
+}
